ea007455b787dca01cdbda57c8335669371d0ebf
Calider <97751771+Calider@users.noreply.github.com>
2022-02-21 13:22:57 +0300

    same chenges

--- test.py
+++ test.py
@@ -2,13 +2,24 @@ s = input().split()
 my_list = []
 my_list.append([])
 n=1
-for i in range(len(s)):
-    my_list.append([s[i]])
-for i in range(0,len(s),n):
-    my_list.append(s[i:n])
+if len(s) == 1:
+    my_list.append(s)
+elif len(s) == 2:
+    for i in range(len(s)):
+        my_list.append([s[i]])
+    my_list.append(s)
+else:
+    for i in range(len(s)):
+        my_list.append([s[i]])
     n += 1
-my_list.append(s)
-print(my_list)
+    k = n
+    for j in range(0, len(s)):
+        my_list.append(s[j:k])
+        k += n
+
+    my_list.append(s)
+# my_list.append(s)
+print(set(my_list))
 
 
 
