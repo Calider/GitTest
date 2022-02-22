ee92b43b20d0978d3b0c32b786d1eaa2c9d080b6
Calider <97751771+Calider@users.noreply.github.com>
2022-02-22 11:38:07 +0300

    matrix 4

--- test.py
+++ test.py
@@ -1,32 +1,17 @@
-s = input().split()
-my_list = []
-my_list.append([])
-n=1
-if len(s) == 1:
-    my_list.append(s)
-elif len(s) == 2:
-    for i in range(len(s)):
-        my_list.append([s[i]])
-    my_list.append(s)
+n = int(input())
+mas = [[int(j) for j in input().split()] for i in range(n)]
+priz = True
+for i in range(n):
+    if priz == False:
+        break
+    else:
+        for j in range(n):
+            if mas[i][j] == mas[j][i]:
+                priz = True
+            else:
+                priz = False
+                break
+if priz == True:
+    print('YES')
 else:
-    for i in range(len(s)):
-        my_list.append([s[i]])
-    n += 1
-    k = n
-    for j in range(0, len(s)):
-        my_list.append(s[j:k])
-        k += n
-
-    my_list.append(s)
-# my_list.append(s)
-print(set(my_list))
-
-
-
-# n = int(input())
-# my_list = []
-# for i in range(n):
-#     elem = [i+1 for i in range(n)]
-#     my_list.append(elem)
-# for i in my_list:
-#     print(i)
+    print('NO')
