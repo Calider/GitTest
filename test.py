12cea4b1fd0720e78375b8baa937503486bf97d2
Calider <97751771+Calider@users.noreply.github.com>
2022-02-23 14:54:58 +0300

    matrix 6

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
-n = int(input())
-mas = [[int(j) for j in input().split()] for i in range(n)]
-for i in range(n//2):
-    mas[i], mas[n-i-1] = mas[n-i-1], mas[i]
-
+n = 8
+mas = [['.']*n for j in range(n)]
+kon = []
+kon.extend(input())
+kor = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7,
+       '8': 0, '7': 1, '6': 2, '5': 3, '4': 4, '3': 5, '2': 6, '1': 7}
+ik = kor[kon[1]]
+jk = kor[kon[0]]
+mas[ik][jk] = 'N'
 for i in range(n):
-    for j in range(n):
-        print(mas[i][j], end=' ')
-    print()
+        for j in range(n):
+            if (jk - j) * (ik - i) == 2 or (jk - j) * (ik - i) == -2:
+                try:
+                    mas[i][j] = '*'
+                except IndexError:
+                    None
 
+def print_matrix(matrix):
+    for i in range(n):
+        for j in range(n):
+            print(matrix[i][j], end=' ')
+        print()
 
-
+print_matrix(mas)
