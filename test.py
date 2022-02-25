00e9e52d0d182d3a34581129d6b6c7ddc6fd6ac9
Calider <97751771+Calider@users.noreply.github.com>
2022-02-25 12:03:11 +0300

    matrix ulit

--- test.py
+++ test.py
@@ -1,24 +1,38 @@
-n = 8
-mas = [['.']*n for j in range(n)]
-kon = []
-kon.extend(input())
-kor = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7,
-       '8': 0, '7': 1, '6': 2, '5': 3, '4': 4, '3': 5, '2': 6, '1': 7}
-ik = kor[kon[1]]
-jk = kor[kon[0]]
-mas[ik][jk] = 'N'
-for i in range(n):
-        for j in range(n):
-            if (jk - j) * (ik - i) == 2 or (jk - j) * (ik - i) == -2:
-                try:
-                    mas[i][j] = '*'
-                except IndexError:
-                    None
-
-def print_matrix(matrix):
+vvod = input().split()
+n = int(vvod[0])
+m = int(vvod[1])
+nn = 1
+mas = [[0 for j in range(m)] for i in range(n)]
+for j in range(m):
+    mas[0][j] = nn
+    nn += 1
+i = 0
+j = 0
+mn = n
+nm = m
+while nn <= n * m:
     for i in range(n):
-        for j in range(n):
-            print(matrix[i][j], end=' ')
+        if mas[i][m - 1] == 0:
+            mas[i][m - 1] = nn
+            nn += 1
+    for j in range(m):
+        if mas[n - 1][m-j-1] == 0:
+            mas[n - 1][m-j-1] = nn
+            nn += 1
+    for i in range(n - 1):
+        if mas[i - n + 1][mn - n] == 0:
+            mas[i - n + 1][mn - n] = nn
+            nn += 1
+    for j in range(m - 1):
+        if mas[nm - n - 1][j] == 0:
+            mas[nm - n - 1][j] = nn
+            nn += 1
+    n -= 2
+    m -= 2
+def print_matrix(matrix):
+    for i in range(mn):
+        for j in range(nm):
+            print(str(matrix[i][j]).ljust(3), end='')
         print()
 
 print_matrix(mas)
